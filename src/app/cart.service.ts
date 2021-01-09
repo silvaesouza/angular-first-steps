5a0f175626ded3aaa43cee9aa3a4340c45663080
silvaesouza <silvaesouza@gmail.com>
2021-01-09 01:28:26 -0300

    Configure CartService to use HttpClient

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from "@angular/core";
 
+import { HttpClient } from "@angular/common/http";
+
 @Injectable({
   providedIn: "root"
 })
 export class CartService {
   items = [];
+
+  constructor(private http: HttpClient) {}
 
   addToCart(product) {
     this.items.push(product);
